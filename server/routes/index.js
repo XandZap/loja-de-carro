23f412c89d5be87d2ad873eb3c8fa89ee7ec6cf8
Alexandre Roberto <alexandreroberto.94@gmail.com>
2022-03-10 18:22:05 -0400

    feat: Challenge 33 Complete

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,27 +1,31 @@
-'use strict';
+"use strict";
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var data = [];
 
-router.get('/', function(req, res) {
-  console.log('[GET] /car:', data)
+router.get("/", function (req, res) {
+  console.log("[GET] /car:", data);
   res.json(data);
 });
 
-router.post('/', function(req, res) {
+router.post("/", function (req, res) {
   data.push({
     image: req.body.image,
     brandModel: req.body.brandModel,
     year: req.body.year,
     plate: req.body.plate,
-    color: req.body.color
+    color: req.body.color,
   });
-  console.log('[POST] /car:', JSON.stringify({
-    body: req.body,
-    data
-  }, null, 2))
-  res.json({ message: 'success' });
+  res.json({ message: "success" });
+});
+
+router.delete("/", function (req, res) {
+  data = data.filter(function (car) {
+    if (car.plate !== req.body.plate) console.log("[DEL] /car:", car);
+    return car.plate !== req.body.plate;
+  });
+  res.json({ message: "success" });
 });
 
 module.exports = router;
